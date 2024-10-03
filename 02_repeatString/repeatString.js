d26be434264c6d53f14e1f47402405312bc3f796
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-03 18:31:29 +0100

    repeatString.js: wrote function. Now passes first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let toReturn = "";
+    for (i = 1; i <= num; i++) {
+        toReturn+= string;
+    }
+    return toReturn;
 };
 
 // Do not edit below this line
