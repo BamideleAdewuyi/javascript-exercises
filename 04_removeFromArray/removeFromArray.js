45b3d2f511c71a191286c2505edeabf2687db1c4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 13:11:04 +0100

    removeFromArray.js: Programmed solution, passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+
+    
+    for (i = 1; i < arguments.length; i++) {
+        arr.splice(arr.indexOf(arguments[i]), 1);
+    }
+    
+    console.log(arr);
+    
+    return arr;
 };
 
 // Do not edit below this line
