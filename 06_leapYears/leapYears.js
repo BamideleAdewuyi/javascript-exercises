d7815a8a2f8003b5064cb38bcd623d6acb367e36
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 22:34:08 +0100

    leapYears.js: Added switch statement. Passed first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    switch(year) {
+        case (year % 100 === 0 && year % 4 != 0):
+            return false;
+        case (year % 0 != 0):
+            return false;
+        default:
+            return true;
+    }
 };
 
 // Do not edit below this line
