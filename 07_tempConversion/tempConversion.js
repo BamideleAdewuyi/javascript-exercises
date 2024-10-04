a28312395608fafbad03bc40f50ccd09ce586636
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 23:16:35 +0100

    tempConverstion.js: Wrote functions. Passes first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let temperature = Number(((F - 32) * 1.8).toFixed(1));
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let temperature = Number(((C + 32) * 1.8).toFixed(1));
+  return temperature;
 };
 
 // Do not edit below this line
