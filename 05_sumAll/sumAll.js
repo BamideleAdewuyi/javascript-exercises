21f03384d45d9513cd2af13a3647c551abd0bcd0
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 21:52:15 +0100

    sumAll.js: Added if statement to deal with non-integer arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,14 +2,20 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
-    let arr = [num1, num2].sort();
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        let arr = [num1, num2].sort();
 
-    let total = 0;
-    for (i = arr[0]; i <= arr[1]; i++) {
-        total += i;
+        let total = 0;
+        for (i = arr[0]; i <= arr[1]; i++) {
+            total += i;
+        }
+
+        return total;
     }
-
-    return total;
+    else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
