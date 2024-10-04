c44d02286ef3ffbaf4dc0948f249878d3eabb8c4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 22:44:19 +0100

    leapYears.js: changed switch statement to if statement. Now passes first two tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,17 @@
 const leapYears = function(year) {
-    switch(year) {
-        case (year % 100 === 0 && year % 4 != 0):
-            return false;
-        case (year % 0 != 0):
-            return false;
-        default:
-            return true;
+
+    if (year % 100 === 0 && year % 4 != 0) {
+        return false;
     }
+
+    else if (year % 4 != 0) {
+        return false;
+    }
+
+    else {
+        return true;
+    }
+
 };
 
 // Do not edit below this line
