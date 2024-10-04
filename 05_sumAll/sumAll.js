68b7101fdaf503dd6e54189e617e7ee6a66fd2ff
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 21:25:35 +0100

    sumAll.js: edited for loop so i runs from arr[0] to arr[1]

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1, num2) {
     let arr = [num1, num2].sort();
 
     let total = 0;
-    for (i = num1; i <= num2; i++) {
+    for (i = arr[0]; i <= arr[1]; i++) {
         total += i;
     }
 
