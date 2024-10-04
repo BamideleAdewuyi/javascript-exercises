2d3dfd464801ed60194f36011465bc29a5e989be
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-04 21:22:04 +0100

    sumAll.js: wrote function. Passed first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let arr = [num1, num2].sort();
 
+    let total = 0;
+    for (i = num1; i <= num2; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
