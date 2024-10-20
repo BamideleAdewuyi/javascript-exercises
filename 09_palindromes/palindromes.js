0b87f4846aed61a9095e72a0627b0456fb2f6113
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-20 22:28:48 +0100

    Filled in palindromes function. Passed first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const punctuationless = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let arr = punctuationless.split("");
+    if  (arr == arr.reverse()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
