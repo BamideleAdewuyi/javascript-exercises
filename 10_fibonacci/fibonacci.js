e5c78672fa05c9277d8c618dc5c0ead284109165
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-20 23:26:44 +0100

    fibonacci.js: Changed fibonacci function to convert input to number before comparisons

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,15 +1,16 @@
 const fibonacci = function(num) {
-    if (num === 0) {
+    const a = Number(num);
+    if (a === 0) {
         return 0;
     }
-    if (num < 0) {
+    if (a < 0) {
         return "OOPS";
     }
     let arr = [1, 1];
-    for (i = 1; i < num; i++) {
+    for (i = 1; i < a; i++) {
         arr.push(arr[i] + arr[i-1]);
     }
-    return arr[num - 1];
+    return arr[a - 1];
 };
 
 // Do not edit below this line
